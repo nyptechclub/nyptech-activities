a6056c7f1d10ade0d6798158b9eef6381fe1b9bf
Dennise Catolos <dentolos19@gmail.com>
2024-08-27 11:25:44 +0800

    Updated sections

--- src/app/solve/_sections/how.tsx
+++ src/app/solve/_sections/how.tsx
@@ -1,7 +1,10 @@
+import { unbounded } from "@/fonts";
+import clsx from "clsx";
+
 export default function HowSection() {
   return (
     <main className={"px-8 h-[60%] flex flex-col items-center justify-center text-center"}>
-      <div className={"mb-4 font-bold text-4xl text-white"}>
+      <div className={clsx("mb-4 font-bold text-4xl text-white", unbounded.className)}>
         2 workshops, 2 updates every week
       </div>
       <div className={"max-w-xl text-lg"}>It&apos;s pretty simple!</div>
--- src/app/solve/_sections/session.tsx
+++ src/app/solve/_sections/session.tsx
@@ -1,11 +1,17 @@
+import { unbounded } from "@/fonts";
+import clsx from "clsx";
 import Link from "next/link";
 
 export default function SessionSection() {
   return (
     <main className={"px-8 h-[60%] flex flex-col items-center justify-center text-center"}>
-      <div className={"mb-4 font-bold text-4xl text-white"}>Still not convince? Got questions?</div>
-      <div className={"max-w-xl text-lg"}>Join our virtual information sharing session on 2nd September.</div>
-      <Link className={"mt-8 btn btn-primary"} href={"#"}>Join now!</Link>
+      <div className={clsx("mb-4 font-bold text-4xl text-white", unbounded.className)}>
+        Still not convinced? Got questions?
+      </div>
+      <div className={"max-w-xl text-lg"}>Join our virtual information sharing session on the 2nd of September.</div>
+      <Link className={"mt-8 btn btn-primary"} href={"https://calendar.app.google/jxGB8CDUFbmnpgkP6"}>
+        Join now!
+      </Link>
     </main>
   );
 }
