e64e78e5cf12beecb80cc277ae4358c5119beab7
Tan Sven <iamsven2005@gmail.com>
2024-09-09 08:31:21 +0800

    Fix route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/link(.*)", "/room(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/link(.*)", "/room(.*)", "/passwords(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
